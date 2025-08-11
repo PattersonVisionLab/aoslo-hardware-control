c890e50261699a5a19eff3d2458ba3704d724aea
1P-AOSLO <spatte16@ur.rochester.edu>
2025-08-11 17:50:39 -0400

    serial numbers

---
+++ src/+ur/+pattersonlab/+aoslo/+motion/Controllers_1PAOSLO.m
@@ -0,0 +1,22 @@
+classdef Controllers_1PAOSLO < double
+% Serial numbers for 1P AOSLO controllers
+
+    enumeration 
+        MUSTANG     (83850011)
+        TOPTICA     (83849957)
+        REFLECTANCE (27006819)
+        
+        REF_X       (83857268)
+        REF_Y       (83855258)
+        REF_Z       (26250117)
+
+        VIS_X       (83848285)
+        VIS_Y       (83848287)
+        VIS_Z       (83848308)
+
+        FIBER_X     (26005133)
+        FIBER_Y     (26004981)
+        FIBER_Z     (26005106)
+    end
+end
+
